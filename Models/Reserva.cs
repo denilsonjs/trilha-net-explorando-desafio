7f81cf1d73a3e9b8b0122ec757889d57a8be0164
denilsonjs <47991143+denilsonjs@users.noreply.github.com>
2024-11-03 12:44:01 -0300

    Solução
    Denilson

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,13 +17,14 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if ( Suite.Capacidade >= hospedes.Count )
             {
                 Hospedes = hospedes;
             }
             else
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                throw new Exception("Numero de hóspedes superior a capacidade de suíte .");
                 // *IMPLEMENTE AQUI*
             }
         }
@@ -37,7 +38,7 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count ;
         }
 
         public decimal CalcularValorDiaria()
@@ -46,12 +47,16 @@ namespace DesafioProjetoHospedagem.Models
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
             decimal valor = 0;
+            decimal desconto = 0.90M;
+
+            valor = DiasReservados * Suite.ValorDiaria;
+
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                valor =  valor * desconto;
             }
 
             return valor;
